3ee2b153a37448c0bab426e9bfa0f0f7e118e126
Sabir <sea_bak@hotmail.com>
2018-05-13 15:05:16 -0700

    Update get-FolderTotalsize.ps1

--- src/get-FolderTotalsize.ps1
+++ src/get-FolderTotalsize.ps1
@@ -1,2 +1,1 @@
-"{0:N2} GB"  -f ((Get-ChildItem C:\Users -Recurse | foreach`
-{$totalsize+=$_.length;$totalsize / 1GB}))
+"{0:N2} GB"  -f ((Get-ChildItem C:\Users -Recurse | foreach {$totalsize+=$_.length;$totalsize / 1GB}))
