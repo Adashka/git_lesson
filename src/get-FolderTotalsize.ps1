18818f03926de3953e9bbfe8965bfa37595f848f
Sabir Shukurov <=xxxxx.@yyyy.com>
2019-05-24 12:04:35 -0700

    Changed get-FolderTotalSize.ps1 file

--- src/get-FolderTotalsize.ps1
+++ src/get-FolderTotalsize.ps1
@@ -1,1 +1,5 @@
-"{0:N2} GB"  -f ((Get-ChildItem C:\Users -Recurse | foreach {$totalsize+=$_.length;$totalsize / 1GB}))
+Get-ChildItem "C:\ISOs" -Recurse | foreach {$totalsize +=$_.length};"{0:N2} GB"  -f ($totalsize /1GB)
+
+Write-host "### OR with measure and Sum ###" -ForegroundColor Green
+
+"{0:N2} GB" -f ((Get-ChildItem C:\ISOs -Recurse | Measure-Object -Property Length -Sum -ErrorAction stop).Sum / 1GB)
